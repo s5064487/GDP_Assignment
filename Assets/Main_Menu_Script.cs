cc46ac87d553054113667335f85a8aab845051af
Ricky Hook <s5064487@bournemouth.ac.uk>
2018-05-16 19:41:57 +0100

    Started on Menu

---
+++ Assets/Main_Menu_Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("Rube Goldberg Scence");
+    }
+           
+}
