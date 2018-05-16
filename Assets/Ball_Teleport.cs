2b3b2fbc9eca2a115f6e644b5ee773efd3c242b2
Ricky Hook <s5064487@bournemouth.ac.uk>
2018-05-16 15:28:23 +0100

    Initial Commit

---
+++ Assets/Ball_Teleport.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Ball_Teleport : MonoBehaviour
+{
+
+    public float speed = 0.9f;
+    public Vector3 startpos;
+    public Vector3 endpos;
+
+
+    // Use this for initialization
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Teleporting Ball
+
+    void OnCollisionEnter(Collision col)
+
+    {
+        if (col.gameObject.name == "Box Floor") //When the "Ball" object collides with "Box Floor" object in Teleport_Box the Ball teleports above the ramp. 
+        {
+            transform.position = Vector3.Lerp(endpos, startpos, speed * Time.deltaTime);
+        }
+    }
+}
