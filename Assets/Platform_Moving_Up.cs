3f3d051ee8e4d808e20a8917419795eda39e0f06
Ricky Hook <s5064487@bournemouth.ac.uk>
2018-05-17 20:25:24 +0100

    Added Dominos

--- Assets/Platform_Moving_Up.cs
+++ Assets/Platform_Moving_Up.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Platform_Moving_Up : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public
+    Vector3 current_position;
+    public float speed = 0.9f;
+    public Vector3 startpos;
+    public Vector3 endpos;
+    // Use this for initialization
+    void Start ()
+    {
+        current_position = this.transform.position;	
 	}
 
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.name == "Button");
+        {
+            gameObject.transform.Translate(Vector3.up * 0.125f);
+        }
+    }
+    void Update ()
     
+    {
+        //gameObject.transform.Translate(Vector3.up * 0.125f);
+
+        {
+            //if (gameObject.name == "Button") ; OnCollison);
+        }
+    }
+
+
 }
