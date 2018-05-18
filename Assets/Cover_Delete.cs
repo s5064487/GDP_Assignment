177378c648d8bb07d933fef6c0f0e9594ad0e037
Ricky Hook <s5064487@bournemouth.ac.uk>
2018-05-18 14:23:14 +0100

    Fixed Elevator and Arm Motor Working

---
+++ Assets/Cover_Delete.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover_Delete : MonoBehaviour {
+
+    public GameObject Ball;
+    public GameObject Cover;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == Ball)
+        {
+            Destroy(Cover);
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
